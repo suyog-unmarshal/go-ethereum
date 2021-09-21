3e0f65b8677549fd9f44491404ba2db5d91f8df7
suyog-unmarshal <suyog@unmarshal.io>
2021-09-21 16:40:45 +0530

    added blockreceipts

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -283,6 +283,11 @@ func (ec *Client) TransactionReceipt(ctx context.Context, txHash common.Hash) (*
 	}
 	return r, err
 }
+func (ec *Client) BlockReceipts(ctx context.Context, number *big.Int) (types.Receipts, error) {
+	var result types.Receipts
+	err := ec.c.CallContext(ctx, &result, "eth_getBlockReceipts", toBlockNumArg(number))
+	return result, err
+}
 
 type rpcProgress struct {
 	StartingBlock hexutil.Uint64
